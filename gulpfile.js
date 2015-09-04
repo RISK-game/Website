de9e2cb41f84e54fd88d1c19bda9fe262f9c900b
Sawny <sawny@wallstrom.it>
2015-09-04 20:46:06 +0200

    Added gulp watch, scss and js

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+ 
+// SCSS -> CSS
+gulp.task('sass', function () {
+  gulp.src('./css/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dist'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/main.scss', ['sass']);
+});
+
+
+// Concat files into one so we don't need to add every single JS file
+// to the HTML code
+gulp.task('jsConcat', function() {
+  // The core of angular is currently excluded to make debuing easier.
+  gulp.src(['!./js/vendor/angular.js', './js/**/*.js'])
+    .pipe(concat('main.js', {newLine: '/*concat*/ \n'}))
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('jsConcat:watch', function() {
+  gulp.watch('./js/**/*.js', ['jsConcat']);
+})
+
+
+
+gulp.task('default', ['sass:watch', 'jsConcat:watch']);
