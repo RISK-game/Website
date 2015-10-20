e3d28304606a36f70c55b74901362ed5baf22c94
Filip Lindvall <filip9719@gmail.com>
2015-10-20 11:09:43 +0200

    Added some simple code to test my design

--- js/controllers/chat.js
+++ js/controllers/chat.js
@@ -6,10 +6,16 @@ risk.controller('chat', function($scope, $http, $state, API_URL, Flash) {
   $scope.errors = [];
   $scope.test = "It's alive";
   $scope.messages = [
-    {name:'Filip',image:'http://www.able-sc.org/wp-content/uploads/all-about-me-boy.jpg'},
-    {name:'David',image:'http://www.able-sc.org/wp-content/uploads/all-about-me-boy.jpg'},
-    {name:'Hugo',image:'http://www.able-sc.org/wp-content/uploads/all-about-me-boy.jpg'},
-    {name:'Ludvig',image:'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=='}
+    {
+      isMe: true,
+      username: 'Filip',
+      src:'http://www.7-themes.com/data_images/out/63/6987665-cool-lion-pictures.jpg'
+    },
+    {
+      isMe: false,
+      username: 'Filip',
+      src:'http://www.7-themes.com/data_images/out/63/6987665-cool-lion-pictures.jpg'
+    }
   ];
 });
 
